ae8875aa54abedf0ddbc2962df41b6a906bc44ac
Alex <aleksandr.pantsesnyi@myy.haaga-helia.fi>
2016-11-01 14:57:12 -0700

    Add files via upload

---
+++ init.pp
@@ -0,0 +1,22 @@
+class sshd {
+
+	package { 'openssh-server':
+    		ensure => "installed",
+   	}
+
+	file { '/etc/ssh/sshd_config':
+		content => template("sshd/sshd_config"),
+		require => Package["openssh-server"],
+		notify => Service["ssh"],
+	}
+
+	service { 'ssh':
+    		ensure    => running,
+    		enable    => true,
+    		
+    		require => Package[openssh-server],
+		hasstatus => 'false',
+		status =>"/etc/init.d/ssh status|grep running",
+		
+	}
+}
