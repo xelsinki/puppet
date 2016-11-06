6b96617ebbeccdf005ec97b8623928a3372877c3
Alex <aleksandr.pantsesnyi@myy.haaga-helia.fi>
2016-11-06 13:40:38 +0200

    Update ufw.pp

--- ufw.pp
+++ ufw.pp
@@ -1,15 +1,15 @@
 
 class ufw {
-Exec {
-path => ‘/bin/:/usr/bin/:/sbin/:/usr/sbin/’,
-}
+  Exec {
+    path => '/bin/:/usr/bin/:/sbin/:/usr/sbin/',
+  }
 
-exec { ‘ufw enable’:
-unless => ‘sudo ufw status verbose|grep “Status: active”‘,
-}
+  exec { 'ufw enable':
+    unless => 'sudo ufw status verbose|grep "Status: active"',
+  }
 
-exec { ‘ufw allow 22/tcp’:
-unless => ‘ufw status verbose|grep 22/tcp’,
-}
+  exec { 'ufw allow 22/tcp':
+    unless => 'ufw status verbose|grep 22/tcp',
+  }
 
 }
